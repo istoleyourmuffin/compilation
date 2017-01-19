6f3e13c8c1e26319630f4766b71cabacfd4d4a5d
emiju2 <emilie.arcelli@orange.fr>
2017-01-19 17:09:27 +0100

    ajout de la fonction logique et

--- arbre/expression/EtLogique.java
+++ arbre/expression/EtLogique.java
@@ -19,8 +19,22 @@ public class EtLogique extends BinaireLogique {
 
 	@Override
 	public String toMIPS() {
-		// TODO Auto-generated method stub
-		return null;
+		StringBuilder sb = new StringBuilder();
+		
+		sb.append("# ----- Et Logique ----- \n");
+		sb.append("# Ranger operande gauche dans $v0 \n");
+		sb.append(gauche.toMIPS());
+		sb.append("si1 : beqz $v0, sinon2 \n");
+		sb.append("# Ranger operande droite dans $v0 \n");
+		sb.append(droite.toMIPS());
+		sb.append("si2 : beqz $v0, sinon2 \n");
+		sb.append("alors2 : li $v0, 1 \n");
+		sb.append("j fin \n");
+		sb.append("sinon2 : li $v0, 0 \n");
+		sb.append("fin : \n");
+		sb.append("# ---- Fin Et Logique ---- \n\n");
+		
+		return sb.toString();
 	}
 
 	@Override
