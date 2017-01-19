18e49591d732eee60ad1bff053b605865b306849
Antoine <antoine.gj@gmail.com>
2017-01-19 16:50:26 +0100

    ajout operations de comparaison

--- arbre/expression/Superieur.java
+++ arbre/expression/Superieur.java
@@ -19,8 +19,22 @@ public class Superieur extends Comparaison {
 
 	@Override
 	public String toMIPS() {
-		// TODO Auto-generated method stub
-		return null;
+		
+		StringBuilder sb = new StringBuilder();
+		
+		sb.append("# ----- Supérieur ----- \n");
+		sb.append("# Ranger operande gauche dans $v0 \n");
+		sb.append(gauche.toMIPS());
+		sb.append("sw $v0, 0($sp) \n");
+		sb.append("add $sp, $sp, -4 \n");
+		sb.append("# Ranger operande droite dans $v0 \n");
+		sb.append(droite.toMIPS());
+		sb.append("add $sp, $sp, 4 \n");
+		sb.append("lw $t8, ($sp) \n");
+		sb.append("sgt $v0, $t8, $v0 \n");
+		sb.append("# ---- Fin Supérieur ---- \n\n");
+		
+		return sb.toString();
 	}
 
 	@Override
