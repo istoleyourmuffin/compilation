abc8d6a954e802967c1add04050585936f0cf259
Antoine <antoine.gj@gmail.com>
2017-02-20 20:39:00 +0100

    ajout declaration sans statut

--- arbre/declaration/DeclarationChamp.java
+++ arbre/declaration/DeclarationChamp.java
@@ -20,6 +20,18 @@ public class DeclarationChamp extends Declaration {
 		
 	}
 	
+	public DeclarationChamp(String t, ListeIdentifiant li, int n) {
+		super(n);
+		for(Identifiant i : li) {
+			try {
+				TDS.getInstance().ajouter(t, i.getNom());
+			} catch (DoubleDeclarationException e) {
+				e.printStackTrace();
+			}
+		}
+		
+	}
+	
 	/* Pas besoin de toMIPS comme ce sont des déclarations, il faut juste ajouter les variables àla TDS */
 
 }
